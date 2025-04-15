39a656e86904e32b297f9d06bc2bcdf69a23b229
196398 <196398@usthealthproof.com>
2025-04-15 13:06:13 +0530

    FuncationalInterfaceIntro

--- com/basics/com/exercises/PrintAllCourses.java
+++ com/basics/com/exercises/PrintAllCourses.java
@@ -8,7 +8,9 @@ import java.util.stream.Collectors;
 public class PrintAllCourses {
     public static void main(String[] args) {
         List<String> courses = Arrays.asList("Spring", "Spring Boot", "API", "Microservices using spring", "AWS", "PCP", "Azure", "Docker","Spring JPA Repository");
-        System.out.print(stringLength(courses));
+        List<String> courses2 = Arrays.asList("Spring", "Spring Boot", "API", "Microservices using spring", "AWS", "PCP", "Azure", "Docker","Spring JPA Repository");
+        System.out.println(tupleTwoListUsingLength(courses, courses2));
+
     }
 
     static void printCources(List<String> courses){
@@ -43,6 +45,35 @@ public class PrintAllCourses {
         return courses.stream()
                 .sorted(Comparator.comparing(course -> course.length()))
                 .map(course -> course + " = "+ course.length())
+                .collect(Collectors.toList());
+    }
+
+    static  String delimitedSep(List<String> courses){
+        return courses.stream()
+                .collect(Collectors.joining("|"));
+    }
+
+    static List<String> charSeperated(List<String> courses){
+        return courses.stream()
+                .map(course -> course.split(""))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
+    }
+
+    static List<List<String>> tupleTwoList(List<String> courses, List<String> courses2){
+        return courses.stream()
+                .flatMap(course ->  courses2.stream()
+                        .map(course2 -> List.of(course, course2)))
+                .collect(Collectors.toList());
+    }
+
+    static List<List<String>> tupleTwoListUsingLength(List<String> courses, List<String> courses2){
+        return courses.stream()
+                .flatMap(course ->  courses2.stream()
+                        .filter(course2 -> course2.length() == course.length()) // use this
+                        .map(course2 -> List.of(course, course2)))
+                //.filter(list -> list.get(0).equals(list.get(1)))
+                //.filter(list -> list.get(0).length() == list.get(1).length())  //instead of this we can use the above filter
                 .collect(Collectors.toList());
     }
 }
